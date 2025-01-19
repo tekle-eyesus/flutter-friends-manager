f63ec336658b59582b4eec352f3efb60a7331844
tekle-TMT <tekleeyesus21@gmail.com>
2025-01-19 02:16:05 -0700

    detail screen modified

--- lib/screen/student_detail.dart
+++ lib/screen/student_detail.dart
@@ -28,9 +28,13 @@ class StudentDetail extends StatelessWidget {
         flexibleSpace: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.topLeft,
-                colors: <Color>[Colors.purple.shade800, Colors.blue.shade900]),
+              begin: Alignment.topRight,
+              end: Alignment.topLeft,
+              colors: <Color>[
+                Colors.purple.shade800,
+                Colors.blue.shade900,
+              ],
+            ),
           ),
         ),
         actions: [
@@ -41,48 +45,140 @@ class StudentDetail extends StatelessWidget {
               ))
         ],
       ),
-      body: Stack(
-        clipBehavior: Clip.none,
+      body: Column(
         children: [
-          Container(
-            height: 115,
-            decoration: const BoxDecoration(
-                color: Colors.amber,
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        "https://static.vecteezy.com/system/resources/thumbnails/023/056/329/small_2x/programmer-people-working-laptops-or-smartphones-with-ai-artificial-intelligence-software-engineer-coding-on-laptop-computers-with-technology-icons-and-binary-code-big-data-ai-bot-digital-machine-photo.jpg"))),
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 115,
+                decoration: const BoxDecoration(
+                    color: Colors.amber,
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://static.vecteezy.com/system/resources/thumbnails/023/056/329/small_2x/programmer-people-working-laptops-or-smartphones-with-ai-artificial-intelligence-software-engineer-coding-on-laptop-computers-with-technology-icons-and-binary-code-big-data-ai-bot-digital-machine-photo.jpg"))),
+              ),
+              Positioned(
+                top: 55,
+                left: 140,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage: FileImage(File(student.profileImg)),
+                    ),
+                    Text(
+                      student.fullName,
+                      style: GoogleFonts.getFont(
+                        "Lato",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      student.email,
+                      style: TextStyle(
+                        color: Colors.grey.shade700,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            top: 55,
-            left: 140,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: FileImage(File(student.profileImg)),
+          const SizedBox(
+            height: 130,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Colors.amber,
                 ),
-                Text(
-                  student.fullName,
-                  style: GoogleFonts.getFont(
-                    "Lato",
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.black,
+                child: Expanded(
+                  child: ListTile(
+                    leading: Image.asset(
+                      "assets/icons/birthday.png",
+                      width: 30,
+                      height: 30,
+                    ),
+                    title: Text("BIRTHDAY"),
+                    subtitle: Text(student.dop.year.toString()),
                   ),
                 ),
-                Text(
-                  student.email,
-                  style: TextStyle(
-                    color: Colors.grey.shade700,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+              ),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: Colors.amber,
+                ),
+                child: Expanded(
+                  child: ListTile(
+                    leading: Image.asset(
+                      "assets/icons/dept.png",
+                      width: 30,
+                      height: 30,
+                    ),
+                    title: const Text("Department"),
+                    subtitle: Text(student.department.split(" ")[0]),
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: ListTile(
+              leading: Image.asset(
+                "assets/icons/phone.png",
+                width: 34,
+                height: 34,
+              ),
+              title: const Text("PHONE"),
+              subtitle: Text(student.phone),
+              trailing: Image.asset(
+                "assets/icons/phoned.png",
+                width: 36,
+                height: 36,
+              ),
             ),
           ),
+          ExpansionTile(
+            shape: Border(),
+            title: Text("PLAN"),
+            leading: Image.asset(
+              "assets/icons/plan.png",
+              width: 32,
+              height: 32,
+            ),
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 13),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 380,
+                      child: Text(
+                          "noll anawer following follows bolled ba mvnv nmxmx ksks slls sllsowow eieiuw wiwoqwouw qooqowpq wqowqw wqiu ee qeiueq qoqoo"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
